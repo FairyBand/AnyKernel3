93d2b736af4194bf163cfb2a88d8d6303e149676
FairyBand <zl3004431@gmail.com>
2025-01-29 14:14:22 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,27 +4,45 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sultan Kernel for the Pixel 8/Pro With KernelSU
-do.devicecheck=1
+kernel.string=OKI Kernel from YAAP With KernelSU Next & SUSFS for OP12
+do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=husky
-device.name2=shiba
-supported.versions=15
+device.name1=
+device.name2=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
 # boot image installation
 block=boot;
-is_slot_device=1;
-. tools/ak3-core.sh;
-split_boot;
-flash_boot;
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
 
-# vendor_kernel_boot installation (for dtb)
-block=vendor_kernel_boot;
-is_slot_device=1;
-reset_ak;
-split_boot;
-flash_boot;
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh
+
+kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
+case $kernel_version in
+    5.1*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
+esac
+
+ui_print " " "  -> ksu_supported: $ksu_supported"
+$ksu_supported || abort "  -> Non-GKI device, abort."
+
+# boot install
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
+## end boot install
